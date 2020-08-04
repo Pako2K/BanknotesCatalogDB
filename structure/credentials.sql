bebf89b3826cee83cc9d52c1920c66a67865deec
Pako2K <fdelanuza@gmail.com>
2020-08-04 08:18:03 +0200

    Added new tables, columns and more data

--- structure/credentials.sql
+++ structure/credentials.sql
@@ -5,7 +5,28 @@
 -- Dumped from database version 12.2
 -- Dumped by pg_dump version 12.2
 
--- Started on 2020-05-16 17:34:55
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE credentials;
+--
+-- Name: credentials; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE credentials WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
+
+
+ALTER DATABASE credentials OWNER TO postgres;
+
+\connect credentials
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -23,7 +44,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 202 (class 1259 OID 16522)
 -- Name: cre_credentials; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -33,31 +53,28 @@ CREATE TABLE public.cre_credentials (
     cre_hashed_pwd text NOT NULL,
     cre_mail text NOT NULL,
     cre_is_admin integer NOT NULL,
-    cre_state integer NOT NULL,
-    cre_validation_code text,
     cre_last_connection text,
-    cre_insert_timestamp timestamp without time zone DEFAULT (now())::timestamp(0) without time zone
+    cre_validation_code text,
+    cre_insert_timestamp timestamp without time zone DEFAULT (now())::timestamp(0) without time zone,
+    cre_state integer NOT NULL
 );
 
 
 ALTER TABLE public.cre_credentials OWNER TO postgres;
 
---
--- TOC entry 2820 (class 0 OID 0)
--- Dependencies: 202
--- Name: COLUMN cre_credentials.cre_state; Type: COMMENT; Schema: public; Owner: postgres
---
 
-COMMENT ON COLUMN public.cre_credentials.cre_state IS '-1 = to be validated
-0 = active
-1 = 1 failed validation attempt
-2 = 2 failed validation attempts
-3 = 3 (and final) failed validation attempts => user is invalid
-';
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+-- INSERT DATA
+
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+-- !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 --
--- TOC entry 2688 (class 2606 OID 16529)
 -- Name: cre_credentials cre_credentials_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -66,15 +83,11 @@ ALTER TABLE ONLY public.cre_credentials
 
 
 --
--- TOC entry 2821 (class 0 OID 0)
--- Dependencies: 202
 -- Name: TABLE cre_credentials; Type: ACL; Schema: public; Owner: postgres
 --
 
 GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.cre_credentials TO "AppAPI";
 
-
--- Completed on 2020-05-16 17:34:55
 
 --
 -- PostgreSQL database dump complete
