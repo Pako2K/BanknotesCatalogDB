85f2c8627ef8af99eaca5a2d842d40a8bcb1722d
Pako2K <41144260+Pako2K@users.noreply.github.com>
2024-01-26 20:24:07 +0100

    Update credentials.sql

--- data/credentials.sql
+++ data/credentials.sql
@@ -2,7 +2,5 @@
 -- Data for Name: cre_credentials; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.cre_credentials (cre_username, cre_salt, cre_hashed_pwd, cre_mail, cre_is_admin, cre_last_connection, cre_validation_code, cre_insert_timestamp, cre_state) VALUES ('Admin', 'b82eae16b11eec7ab6fd26af992f608c', '25a0e225174a2aaf846bbc92369ecdc793279b53839bb64069c88d5ac2ec2f01d6fa3d4ad71c8487f1d7e2ec9e9702f7175ec553c9520f65f8db7d39e4b3da82', 'fdelasdasanuza@gasdasdmail.com', 1, 'Monday, 03 August 2020, 13:44', NULL, '2020-05-12 21:27:16.106628', 0);
 INSERT INTO public.cre_credentials (cre_username, cre_salt, cre_hashed_pwd, cre_mail, cre_is_admin, cre_last_connection, cre_validation_code, cre_insert_timestamp, cre_state) VALUES ('blablabla', '5a8cedf20f614ed8873d01ff51572ddd', 'f81662ca6b82c3cffae7cf5277bcdfb36389b7ef52a51d1dee8eb37b3a1f9910beb056dea7762e86bce33a20d126f803d51721793b19277b666aa945d367f206', 'judy.fk@yahoo.com', 0, 'Saturday, 20 June 2020, 18:47', NULL, '2020-06-20 18:33:46', 0);
-INSERT INTO public.cre_credentials (cre_username, cre_salt, cre_hashed_pwd, cre_mail, cre_is_admin, cre_last_connection, cre_validation_code, cre_insert_timestamp, cre_state) VALUES ('Paco', '8001ead312336545d9aa1c1c7a56e325', '2defc4972a3c17ce52405e4586fc31cd396c2ea45dc337bc5f016ddc93e219bfad3dbe2a3779324b9d2b56268459dceb5b17f8efabd58045477d493413b97855', 'fdelanuza@gmail.com', 0, 'Sunday, 26 July 2020, 23:15', NULL, '2020-06-10 20:31:30', 0);
 
