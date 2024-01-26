68b4da3e54c2b55d6c86a1647055ab47a44cec1f
Pako2K <41144260+Pako2K@users.noreply.github.com>
2024-01-26 20:24:38 +0100

    Delete data/credentials.sql

--- data/credentials.sql
+++
@@ -1,6 +0,0 @@
---
--- Data for Name: cre_credentials; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO public.cre_credentials (cre_username, cre_salt, cre_hashed_pwd, cre_mail, cre_is_admin, cre_last_connection, cre_validation_code, cre_insert_timestamp, cre_state) VALUES ('blablabla', '5a8cedf20f614ed8873d01ff51572ddd', 'f81662ca6b82c3cffae7cf5277bcdfb36389b7ef52a51d1dee8eb37b3a1f9910beb056dea7762e86bce33a20d126f803d51721793b19277b666aa945d367f206', 'judy.fk@yahoo.com', 0, 'Saturday, 20 June 2020, 18:47', NULL, '2020-06-20 18:33:46', 0);
-
